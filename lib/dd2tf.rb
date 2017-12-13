229b0e23b7135c0ce81974938ef6f493ffa30bbf
reizist <reizist@gmail.com>
2017-12-13 14:17:23 +0900

    Revert "Implement printing import command from remote to tfstate"
    This reverts commit bc885fd26ab5447f99a4418731727392dcc4a4af.

--- lib/dd2tf.rb
+++ lib/dd2tf.rb
@@ -1,13 +1,6 @@
 require "dd2tf/version"
-require "dd2tf/config"
-
 require "dd2tf/base"
-require "dd2tf/user"
 require "dd2tf/monitor"
 require "dd2tf/timeboard"
-
-require "dd2tf/import/base"
-require "dd2tf/import/user"
-require "dd2tf/import/monitor"
-require "dd2tf/import/timeboard"
-require "dd2tf/import/downtime"
+require "dd2tf/user"
+require "dd2tf/config"
